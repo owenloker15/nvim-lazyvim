fb41f27c82d0b70b176580e6ac3aed3382ad3f98
Owen Loker <owenloker15@gmail.com>
2026-08-30 14:55:48 -0400

    Make tokyonight default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,23 @@
 return {
   {
-    "owenloker15/custom-vsassist.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("custom-vsassist")
-    end,
-  },
-  {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
+  },
+  {
+    "owenloker15/custom-vsassist.nvim",
+    priority = 999,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 997,
+    opts = {},
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
   },
 }
